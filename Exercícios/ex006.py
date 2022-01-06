1426083db2436df909d7ec0978a99250e8a1115a
maxpantoja <96695840+maxpantoja@users.noreply.github.com>
2022-01-06 20:13:53 -0300

    Desafio 4

--- Exercícios/ex006.py
+++ Exercícios/ex006.py
@@ -1,2 +1,11 @@
 #Crie um algorítimo que leia um número e mostre o seu dobro, triplo
 #e raiz quadrada
+n = int(input('Digite um número qualquer: '))
+nd = n * 2
+nt = n * 3
+nr = n ** (1/2)
+
+print('A raíz quadrada desse número é ',nr)
+print('O dobro desse número é ',nd)
+print('O triplo desse número é ',nt)
+
