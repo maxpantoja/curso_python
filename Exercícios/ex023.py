969ae4dc2ec2655499e9056705567500ff942f90
maxpantoja <96695840+maxpantoja@users.noreply.github.com>
2022-05-02 20:32:42 -0300

    Update ex023.py

--- Exercícios/ex023.py
+++ Exercícios/ex023.py
@@ -2,5 +2,5 @@
 # Ex: Digite um número: 1834
 # Unidade:4 / Dezena: 3 / Centena: 8 / Milhar: 1
 n = int(input('Digite um número inteiro de 0 a 9999: '))
-print(f'Milhar: {int(n/1000)}\nCentena: {int(n/1000) * 1000 - int(n/100)}')
+print(f'Milhar: {int(n/1000)}')
 
